d103e8a226a8539c4c6edde10e7b8199129394a5
Andrei Anton-Aanei <andrei.anton-aanei@staff.tuiasi.ro>
2021-09-03 07:43:49 +0300

    added replyable interface for replies to threads

---
+++ app/Models/ReplyAble.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+interface ReplyAble{
+    public function subject(): string;
+    public function replies();
+    public function latestReplies(int $amount=3);
+    public function deleteReplies();
+    public function repliesRelation():MorphMany;
+    public function replyAbleSubject(): string;
+}
