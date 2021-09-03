c01f85769daab021a50fc7c7e98d691f41eb6663
Andrei Anton-Aanei <andrei.anton-aanei@staff.tuiasi.ro>
2021-09-03 08:05:40 +0300

    added hasReplies trait

---
+++ app/Traits/HasReplies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+trait HasReplies{
+    public function replies(){
+        return $this->repliesRelation;
+    }
+    public function latestReplies(int $amount = 3){
+        return $this->repliesRelation()->latest()->limit($amount)->get();
+    }
+    public function deleteReplies(){
+        foreach($this->repliesRelation()->get() as $reply){
+            $reply->delete();
+        }
+        $this->unsetRelation('repliesRelation');
+    }
+    public function repliesRelation(): MorphMany{
+        return $this->morphMany(Reply::class, 'repliesRelation', 'replyable_type', 'replyable_id');
+    }
+}
